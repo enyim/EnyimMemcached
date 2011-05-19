bd17a424bdb8b21616e7d57d7aa192ccabb2633e
U-q42office\jaap <jaap@jaapc2.q42.local>
2011-05-19 12:23:32 +0200

    Caught ObjectDisposedException and others in Timer thread, which crashes w3wp.exe

--- Enyim.Caching/Memcached/Locators/DefaultNodeLocator.cs
+++ Enyim.Caching/Memcached/Locators/DefaultNodeLocator.cs
@@ -6,250 +6,265 @@ using System.Threading;
 
 namespace Enyim.Caching.Memcached
 {
-	/// <summary>
-	/// This is a ketama-like consistent hashing based node locator. Used when no other <see cref="T:IMemcachedNodeLocator"/> is specified for the pool.
-	/// </summary>
-	public sealed class DefaultNodeLocator : IMemcachedNodeLocator, IDisposable
-	{
-		private const int ServerAddressMutations = 100;
+  /// <summary>
+  /// This is a ketama-like consistent hashing based node locator. Used when no other <see cref="T:IMemcachedNodeLocator"/> is specified for the pool.
+  /// </summary>
+  public sealed class DefaultNodeLocator : IMemcachedNodeLocator, IDisposable
+  {
+    private static readonly Enyim.Caching.ILog log = Enyim.Caching.LogManager.GetLogger(typeof(DefaultNodeLocator));
 
-		// holds all server keys for mapping an item key to the server consistently
-		private uint[] keys;
-		// used to lookup a server based on its key
-		private Dictionary<uint, IMemcachedNode> servers = new Dictionary<uint, IMemcachedNode>(new UIntEqualityComparer());
 
-		private Timer isAliveTimer;
-		private Dictionary<IMemcachedNode, bool> deadServers = new Dictionary<IMemcachedNode, bool>();
-		private List<IMemcachedNode> allServers = new List<IMemcachedNode>();
+    private const int ServerAddressMutations = 100;
 
-		private ReaderWriterLockSlim serverAccessLock = new ReaderWriterLockSlim();
+    // holds all server keys for mapping an item key to the server consistently
+    private uint[] keys;
+    // used to lookup a server based on its key
+    private Dictionary<uint, IMemcachedNode> servers = new Dictionary<uint, IMemcachedNode>(new UIntEqualityComparer());
 
-		public TimeSpan DeadTimeout { get; set; }
+    private Timer isAliveTimer;
+    private Dictionary<IMemcachedNode, bool> deadServers = new Dictionary<IMemcachedNode, bool>();
+    private List<IMemcachedNode> allServers = new List<IMemcachedNode>();
 
-		/// <summary>
-		/// Checks if a dead node is working again.
-		/// </summary>
-		/// <param name="state"></param>
-		private void callback_isAliveTimer(object state)
-		{
-			this.serverAccessLock.EnterUpgradeableReadLock();
+    private ReaderWriterLockSlim serverAccessLock = new ReaderWriterLockSlim();
 
-			try
-			{
-				if (this.deadServers.Count == 0)
-					return;
+    public TimeSpan DeadTimeout { get; set; }
 
-				List<IMemcachedNode> resurrectList = (from node in this.deadServers.Keys
-													  where node.Ping()
-													  select node).ToList();
+    /// <summary>
+    /// Checks if a dead node is working again.
+    /// </summary>
+    /// <param name="state"></param>
+    private void callback_isAliveTimer(object state)
+    {
+      // This code runs on a timer thread.
+      // We've seen object disposed exceptions raised from serverAccessLock.EnterUpgradeableReadLock()
+      // This is because this class' Dispose method is called.
+      // because of the thread, this code should NEVER 
+      try
+      {
+        this.serverAccessLock.EnterUpgradeableReadLock();
 
-				if (resurrectList.Count > 0)
-				{
-					this.serverAccessLock.EnterWriteLock();
+        try
+        {
+          if (this.deadServers.Count == 0)
+            return;
 
-					try
-					{
-						var stillDead = this.deadServers.Keys.Where(node => !node.Ping());
-						var workingServers = allServers.Except(stillDead).ToList();
+          List<IMemcachedNode> resurrectList = (from node in this.deadServers.Keys
+                                                where node.Ping()
+                                                select node).ToList();
 
-						this.BuildIndex(workingServers);
-					}
-					finally
-					{
-						this.serverAccessLock.ExitWriteLock();
-					}
-				}
+          if (resurrectList.Count > 0)
+          {
+            this.serverAccessLock.EnterWriteLock();
 
-				// ask the timer to fire again after DeadTimeout time
-				this.isAliveTimer.Change((long)this.DeadTimeout.TotalMilliseconds, Timeout.Infinite);
-			}
-			finally
-			{
-				this.serverAccessLock.ExitUpgradeableReadLock();
-			}
-		}
+            try
+            {
+              var stillDead = this.deadServers.Keys.Where(node => !node.Ping());
+              var workingServers = allServers.Except(stillDead).ToList();
 
-		private void BuildIndex(List<IMemcachedNode> nodes)
-		{
-			var keys = new uint[nodes.Count * DefaultNodeLocator.ServerAddressMutations];
+              this.BuildIndex(workingServers);
+            }
+            finally
+            {
+              this.serverAccessLock.ExitWriteLock();
+            }
+          }
 
-			int nodeIdx = 0;
+          // ask the timer to fire again after DeadTimeout time
+          this.isAliveTimer.Change((long)this.DeadTimeout.TotalMilliseconds, Timeout.Infinite);
+        }
+        finally
+        {
+          this.serverAccessLock.ExitUpgradeableReadLock();
+        }
+      }
+      catch (Exception ex)
+      {
+        log.Fatal("Fatal exception in callback_isAliveTimer", ex);
+        return;
+      }
+    }
 
-			foreach (IMemcachedNode node in nodes)
-			{
-				var tmpKeys = DefaultNodeLocator.GenerateKeys(node, DefaultNodeLocator.ServerAddressMutations);
+    private void BuildIndex(List<IMemcachedNode> nodes)
+    {
+      var keys = new uint[nodes.Count * DefaultNodeLocator.ServerAddressMutations];
 
-				for (var i = 0; i < tmpKeys.Length; i++)
-				{
-					this.servers[tmpKeys[i]] = node;
-				}
+      int nodeIdx = 0;
 
-				tmpKeys.CopyTo(keys, nodeIdx);
-				nodeIdx += DefaultNodeLocator.ServerAddressMutations;
-			}
+      foreach (IMemcachedNode node in nodes)
+      {
+        var tmpKeys = DefaultNodeLocator.GenerateKeys(node, DefaultNodeLocator.ServerAddressMutations);
 
-			Array.Sort<uint>(keys);
-			Interlocked.Exchange(ref this.keys, keys);
-		}
+        for (var i = 0; i < tmpKeys.Length; i++)
+        {
+          this.servers[tmpKeys[i]] = node;
+        }
 
-		void IMemcachedNodeLocator.Initialize(IList<IMemcachedNode> nodes)
-		{
-			this.serverAccessLock.EnterWriteLock();
+        tmpKeys.CopyTo(keys, nodeIdx);
+        nodeIdx += DefaultNodeLocator.ServerAddressMutations;
+      }
 
-			try
-			{
-				this.allServers = nodes.ToList();
-				this.BuildIndex(this.allServers);
+      Array.Sort<uint>(keys);
+      Interlocked.Exchange(ref this.keys, keys);
+    }
 
-				if (this.isAliveTimer == null)
-					this.isAliveTimer = new Timer(this.callback_isAliveTimer, null, (long)this.DeadTimeout.TotalMilliseconds, Timeout.Infinite);
-			}
-			finally
-			{
-				this.serverAccessLock.ExitWriteLock();
-			}
-		}
+    void IMemcachedNodeLocator.Initialize(IList<IMemcachedNode> nodes)
+    {
+      this.serverAccessLock.EnterWriteLock();
 
-		IMemcachedNode IMemcachedNodeLocator.Locate(string key)
-		{
-			if (key == null) throw new ArgumentNullException("key");
+      try
+      {
+        this.allServers = nodes.ToList();
+        this.BuildIndex(this.allServers);
 
-			this.serverAccessLock.EnterUpgradeableReadLock();
+        if (this.isAliveTimer == null)
+          this.isAliveTimer = new Timer(this.callback_isAliveTimer, null, (long)this.DeadTimeout.TotalMilliseconds, Timeout.Infinite);
+      }
+      finally
+      {
+        this.serverAccessLock.ExitWriteLock();
+      }
+    }
 
-			try { return this.Locate(key); }
-			finally { this.serverAccessLock.ExitUpgradeableReadLock(); }
-		}
+    IMemcachedNode IMemcachedNodeLocator.Locate(string key)
+    {
+      if (key == null) throw new ArgumentNullException("key");
 
-		IEnumerable<IMemcachedNode> IMemcachedNodeLocator.GetWorkingNodes()
-		{
-			this.serverAccessLock.EnterReadLock();
+      this.serverAccessLock.EnterUpgradeableReadLock();
 
-			try { return this.allServers.Except(this.deadServers.Keys).ToArray(); }
-			finally { this.serverAccessLock.ExitReadLock(); }
-		}
+      try { return this.Locate(key); }
+      finally { this.serverAccessLock.ExitUpgradeableReadLock(); }
+    }
 
-		private IMemcachedNode Locate(string key)
-		{
-			var node = FindNode(key);
-			if (node == null || node.IsAlive)
-				return node;
+    IEnumerable<IMemcachedNode> IMemcachedNodeLocator.GetWorkingNodes()
+    {
+      this.serverAccessLock.EnterReadLock();
 
-			// move the current node to the dead list and rebuild the indexes
-			this.serverAccessLock.EnterWriteLock();
+      try { return this.allServers.Except(this.deadServers.Keys).ToArray(); }
+      finally { this.serverAccessLock.ExitReadLock(); }
+    }
 
-			try
-			{
-				// check if it's still dead or it came back
-				// while waiting for the write lock
-				if (!node.IsAlive)
-					this.deadServers[node] = true;
+    private IMemcachedNode Locate(string key)
+    {
+      var node = FindNode(key);
+      if (node == null || node.IsAlive)
+        return node;
 
-				this.BuildIndex(this.allServers.Except(this.deadServers.Keys).ToList());
-			}
-			finally
-			{
-				this.serverAccessLock.ExitWriteLock();
-			}
+      // move the current node to the dead list and rebuild the indexes
+      this.serverAccessLock.EnterWriteLock();
 
-			// try again with the dead server removed from the lists
-			return this.Locate(key);
-		}
+      try
+      {
+        // check if it's still dead or it came back
+        // while waiting for the write lock
+        if (!node.IsAlive)
+          this.deadServers[node] = true;
 
-		/// <summary>
-		/// locates a node by its key
-		/// </summary>
-		/// <param name="key"></param>
-		/// <returns></returns>
-		private IMemcachedNode FindNode(string key)
-		{
-			if (this.keys.Length == 0) return null;
+        this.BuildIndex(this.allServers.Except(this.deadServers.Keys).ToList());
+      }
+      finally
+      {
+        this.serverAccessLock.ExitWriteLock();
+      }
 
-			uint itemKeyHash = BitConverter.ToUInt32(new FNV1a().ComputeHash(Encoding.UTF8.GetBytes(key)), 0);
-			// get the index of the server assigned to this hash
-			int foundIndex = Array.BinarySearch<uint>(this.keys, itemKeyHash);
+      // try again with the dead server removed from the lists
+      return this.Locate(key);
+    }
 
-			// no exact match
-			if (foundIndex < 0)
-			{
-				// this is the nearest server in the list
-				foundIndex = ~foundIndex;
+    /// <summary>
+    /// locates a node by its key
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns></returns>
+    private IMemcachedNode FindNode(string key)
+    {
+      if (this.keys.Length == 0) return null;
 
-				if (foundIndex == 0)
-				{
-					// it's smaller than everything, so use the last server (with the highest key)
-					foundIndex = this.keys.Length - 1;
-				}
-				else if (foundIndex >= this.keys.Length)
-				{
-					// the key was larger than all server keys, so return the first server
-					foundIndex = 0;
-				}
-			}
+      uint itemKeyHash = BitConverter.ToUInt32(new FNV1a().ComputeHash(Encoding.UTF8.GetBytes(key)), 0);
+      // get the index of the server assigned to this hash
+      int foundIndex = Array.BinarySearch<uint>(this.keys, itemKeyHash);
 
-			if (foundIndex < 0 || foundIndex > this.keys.Length)
-				return null;
+      // no exact match
+      if (foundIndex < 0)
+      {
+        // this is the nearest server in the list
+        foundIndex = ~foundIndex;
 
-			return this.servers[this.keys[foundIndex]];
-		}
+        if (foundIndex == 0)
+        {
+          // it's smaller than everything, so use the last server (with the highest key)
+          foundIndex = this.keys.Length - 1;
+        }
+        else if (foundIndex >= this.keys.Length)
+        {
+          // the key was larger than all server keys, so return the first server
+          foundIndex = 0;
+        }
+      }
 
-		private static uint[] GenerateKeys(IMemcachedNode node, int numberOfKeys)
-		{
-			const int KeyLength = 4;
-			const int PartCount = 1; // (ModifiedFNV.HashSize / 8) / KeyLength; // HashSize is in bits, uint is 4 byte long
+      if (foundIndex < 0 || foundIndex > this.keys.Length)
+        return null;
 
-			var k = new uint[PartCount * numberOfKeys];
+      return this.servers[this.keys[foundIndex]];
+    }
 
-			// every server is registered numberOfKeys times
-			// using UInt32s generated from the different parts of the hash
-			// i.e. hash is 64 bit:
-			// 00 00 aa bb 00 00 cc dd
-			// server will be stored with keys 0x0000aabb & 0x0000ccdd
-			// (or a bit differently based on the little/big indianness of the host)
-			string address = node.EndPoint.ToString();
-			var fnv = new FNV1a();
+    private static uint[] GenerateKeys(IMemcachedNode node, int numberOfKeys)
+    {
+      const int KeyLength = 4;
+      const int PartCount = 1; // (ModifiedFNV.HashSize / 8) / KeyLength; // HashSize is in bits, uint is 4 byte long
 
-			for (int i = 0; i < numberOfKeys; i++)
-			{
-				byte[] data = fnv.ComputeHash(Encoding.ASCII.GetBytes(String.Concat(address, "-", i)));
+      var k = new uint[PartCount * numberOfKeys];
 
-				for (int h = 0; h < PartCount; h++)
-				{
-					k[i * PartCount + h] = BitConverter.ToUInt32(data, h * KeyLength);
-				}
-			}
+      // every server is registered numberOfKeys times
+      // using UInt32s generated from the different parts of the hash
+      // i.e. hash is 64 bit:
+      // 00 00 aa bb 00 00 cc dd
+      // server will be stored with keys 0x0000aabb & 0x0000ccdd
+      // (or a bit differently based on the little/big indianness of the host)
+      string address = node.EndPoint.ToString();
+      var fnv = new FNV1a();
 
-			return k;
-		}
+      for (int i = 0; i < numberOfKeys; i++)
+      {
+        byte[] data = fnv.ComputeHash(Encoding.ASCII.GetBytes(String.Concat(address, "-", i)));
 
-		#region [ IDisposable                  ]
+        for (int h = 0; h < PartCount; h++)
+        {
+          k[i * PartCount + h] = BitConverter.ToUInt32(data, h * KeyLength);
+        }
+      }
 
-		void IDisposable.Dispose()
-		{
-			using (this.serverAccessLock)
-			{
-				this.serverAccessLock.EnterWriteLock();
+      return k;
+    }
 
-				try
-				{
-					this.isAliveTimer.Change(Timeout.Infinite, Timeout.Infinite);
-					this.isAliveTimer.Dispose();
-					this.isAliveTimer = null;
+    #region [ IDisposable                  ]
 
-					// all pending operations will fail (not nice but does the job)
-					this.allServers = null;
-					this.servers = null;
-					this.keys = null;
-					this.deadServers = null;
-				}
-				finally
-				{
-					this.serverAccessLock.ExitWriteLock();
-				}
-			}
-		}
+    void IDisposable.Dispose()
+    {
+      using (this.serverAccessLock)
+      {
+        this.serverAccessLock.EnterWriteLock();
 
-		#endregion
-	}
+        try
+        {
+          this.isAliveTimer.Change(Timeout.Infinite, Timeout.Infinite);
+          this.isAliveTimer.Dispose();
+          this.isAliveTimer = null;
+
+          // all pending operations will fail (not nice but does the job)
+          this.allServers = null;
+          this.servers = null;
+          this.keys = null;
+          this.deadServers = null;
+        }
+        finally
+        {
+          this.serverAccessLock.ExitWriteLock();
+        }
+      }
+    }
+
+    #endregion
+  }
 }
 
 #region [ License information          ]
