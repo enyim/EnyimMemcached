23ebf992ae2c551e01307a312be3a48ad1763dc1
jzablocki <dev@zblock.net>
2013-01-02 16:29:31 -0500

    NCBC-166: Allow full fidelity of keys

--- Enyim.Caching/Memcached/KeyTransformers/LegacyMemcachedKeyTransformer.cs
+++ Enyim.Caching/Memcached/KeyTransformers/LegacyMemcachedKeyTransformer.cs
@@ -1,17 +1,32 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
-namespace Enyim.Caching.Memcached
+namespace Enyim.Caching.Memcached.KeyTransformers
 {
 	/// <summary>
-	/// A transformer that simply returns the original key.
-	/// A valid UTF-8 key is recommended.
+	/// A key transfomer that provides support for legacy memcached key rules
 	/// </summary>
-	public class DefaultKeyTransformer : KeyTransformerBase
+	public class LegacyMemcachedKeyTransformer : KeyTransformerBase
 	{
+		static readonly char[] ForbiddenChars = { 
+			'\u0000', '\u0001', '\u0002', '\u0003',
+			'\u0004', '\u0005', '\u0006', '\u0007',
+			'\u0008', '\u0009', '\u000a', '\u000b',
+			'\u000c', '\u000d', '\u000e', '\u000f',
+			'\u0010', '\u0011', '\u0012', '\u0013',
+			'\u0014', '\u0015', '\u0016', '\u0017',
+			'\u0018', '\u0019', '\u001a', '\u001b',
+			'\u001c', '\u001d', '\u001e', '\u001f',
+			'\u0020'
+		};
+
 		public override string Transform(string key)
 		{
-			//default behavior is to allow all valid strings
+			if (key.IndexOfAny(ForbiddenChars) > -1)
+				throw new ArgumentException("Keys cannot contain the chars 0x00-0x20 and space.");
+
 			return key;
 		}
 	}
@@ -22,7 +37,7 @@ namespace Enyim.Caching.Memcached
  *
  *    @author Couchbase <info@couchbase.com>
  *    @copyright 2012 Couchbase, Inc.
- *   Copyright (c) 2010 Attila Kisk�, enyim.com
+ *   Copyright (c) 2010 Attila Kiskó, enyim.com
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
