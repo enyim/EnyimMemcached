6a2e643b2c93121414df8a7aa2215c04ddd48ec2
dudu <dudu@cnblogs.com>
2016-09-19 21:40:18 +0800

    Upgrade to .NET Core 1.0.1

--- Enyim.Caching/Memcached/Transcoders/DataContractTranscoder.cs
+++ Enyim.Caching/Memcached/Transcoders/DataContractTranscoder.cs
@@ -1,7 +1,6 @@
 using System;
 using System.IO;
 using System.Runtime.Serialization;
-using System.Runtime.Serialization.Json;
 using System.Text;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Bson;
