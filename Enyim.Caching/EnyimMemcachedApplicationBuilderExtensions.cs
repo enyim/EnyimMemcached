9908a17c203aeed48b9e2facb89ad079893c3e0a
dudu <dudu@cnblogs.com>
2016-10-20 16:19:12 +0800

    Implement app.UseEnyimMemcached() to warm up socket pool

---
+++ Enyim.Caching/EnyimMemcachedApplicationBuilderExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Enyim.Caching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Microsoft.AspNetCore.Builder
+{
+    public static class EnyimMemcachedApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseEnyimMemcached(this IApplicationBuilder app)
+        {
+            try
+            {
+                app.ApplicationServices.GetService<IMemcachedClient>()
+                    .GetAsync<string>("EnyimMemcached").Wait();
+                Console.WriteLine("EnyimMemcached Started.");
+            } catch (Exception ex)
+            {
+                app.ApplicationServices.GetService<ILogger<IMemcachedClient>>()
+                    .LogError(new EventId(), ex, "EnyimMemcached Failed.");
+            }
+            return app;
+        }
+    }
+}
