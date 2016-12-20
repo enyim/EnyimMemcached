1624626f5671e5ae847d485e545f8c755d26fb05
dudu <dudu@cnblogs.com>
2016-12-20 13:51:50 +0800

    Rename TransformSHA1() to ToSHA1Hash()

--- Enyim.Caching/Memcached/KeyTransformers/KeyTransformerUtility.cs
+++ Enyim.Caching/Memcached/KeyTransformers/KeyTransformerUtility.cs
@@ -9,7 +9,7 @@ namespace Enyim.Caching.Memcached.KeyTransformers
 {
     public static class KeyTransformerUtility
     {
-        public static string TransformSHA1(string key)
+        public static string ToSHA1Hash(string key)
         {
             return new SHA1KeyTransformer().Transform(key);
         }
