15881d192d625c97344ac85b0cf6edea4ccffba4
dudu <dudu@cnblogs.com>
2016-12-20 12:00:18 +0800

    Add KeyTransformerUtility

---
+++ Enyim.Caching/Memcached/KeyTransformers/KeyTransformerUtility.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enyim.Caching.Memcached.KeyTransformers
+{
+    public static class KeyTransformerUtility
+    {
+        public static string TransformSHA1(string key)
+        {
+            return new SHA1KeyTransformer().Transform(key);
+        }
+    }
+}
