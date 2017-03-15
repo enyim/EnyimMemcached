e4d4b9f3cd67be98195c91b0812e17a2f2fe6e7d
dudu <dudu@cnblogs.com>
2017-03-15 11:19:21 +0800

    Remove unused files

--- build/properties.ps1
+++
@@ -1,6 +0,0 @@
-$buildParams =  @{
-	"solution_name" = "Enyim.Caching.sln";
-	"projects" = @( "Enyim.Caching" );
-	"extras" = @{ "Enyim.Caching.Log4NetAdapter" = "log4net"; "Enyim.Caching.NLogAdapter" = "NLog" };
-	"packages" = @( "Enyim.Caching", "Enyim.Caching.Log4NetAdapter", "Enyim.Caching.NLogAdapter" );
-}
