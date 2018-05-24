b0c2acf8cad8bfcf53485cbc95ff13c78ac93965
dudu <dudu@cnblogs.com>
2018-05-24 14:39:49 +0800

    Fix test failure

--- MemcachedTest/MemcachedClientTest.cs
+++ MemcachedTest/MemcachedClientTest.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 using System.Linq;
+using Microsoft.Extensions.Configuration;
+using System.IO;
 
 namespace MemcachedTest
 {
@@ -19,6 +21,8 @@ namespace MemcachedTest
 
         protected virtual MemcachedClient GetClient(MemcachedProtocol protocol = MemcachedProtocol.Binary, bool useBinaryFormatterTranscoder = false)
         {
+            var configuration = new ConfigurationBuilder().Build();
+
             IServiceCollection services = new ServiceCollection();
             services.AddEnyimMemcached(options =>
             {
@@ -29,11 +33,12 @@ namespace MemcachedTest
                 //    options.Transcoder = "BinaryFormatterTranscoder";
                 //}
             });
-            if(useBinaryFormatterTranscoder)
+            if (useBinaryFormatterTranscoder)
             {
-                services.AddSingleton<ITranscoder,BinaryFormatterTranscoder>();
+                services.AddSingleton<ITranscoder, BinaryFormatterTranscoder>();
             }
 
+            services.AddSingleton<IConfiguration>(configuration);
             services.AddLogging(builder => builder.SetMinimumLevel(LogLevel.Error).AddConsole());
 
             IServiceProvider serviceProvider = services.BuildServiceProvider();
