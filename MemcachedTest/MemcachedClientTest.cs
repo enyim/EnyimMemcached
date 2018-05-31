d364ae6aa639d9b2106225ef08b62e0d010b6854
dudu <dudu@cnblogs.com>
2018-05-31 22:12:28 +0800

    Revert change to fix test failures

--- MemcachedTest/MemcachedClientTest.cs
+++ MemcachedTest/MemcachedClientTest.cs
@@ -9,8 +9,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 using System.Linq;
-using Microsoft.Extensions.Configuration;
-using System.IO;
 
 namespace MemcachedTest
 {
@@ -31,13 +29,11 @@ namespace MemcachedTest
                 //    options.Transcoder = "BinaryFormatterTranscoder";
                 //}
             });
-            if (useBinaryFormatterTranscoder)
+            if(useBinaryFormatterTranscoder)
             {
-                services.AddSingleton<ITranscoder, BinaryFormatterTranscoder>();
+                services.AddSingleton<ITranscoder,BinaryFormatterTranscoder>();
             }
 
-            var configuration = new ConfigurationBuilder().Build();
-            services.AddSingleton<IConfiguration>(configuration);
             services.AddLogging(builder => builder.SetMinimumLevel(LogLevel.Error).AddConsole());
 
             IServiceProvider serviceProvider = services.BuildServiceProvider();
