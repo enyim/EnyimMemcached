b6f26f9e7d0352d37cb5c11897bf25ff7f0b2a56
dudu <dudu@cnblogs.com>
2018-09-20 14:30:42 +0800

    Rewrite SampleWebApp and add integration tests

---
+++ SampleWebApp/Services/BlogPostService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Enyim.Caching.SampleWebApp.Models;
+
+namespace Enyim.Caching.SampleWebApp.Services
+{
+    public class BlogPostService : IBlogPostService
+    {
+        public async ValueTask<IEnumerable<BlogPost>> GetRecent(int itemCount)
+        {
+            return new List<BlogPost> { new BlogPost { Title = "Hello World", Body = "EnyimCachingCore" } };
+        }
+    }
+}
---
+++ SampleWebApp/Services/IBlogPostService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Enyim.Caching.SampleWebApp.Models;
+
+namespace Enyim.Caching.SampleWebApp.Services
+{
+    public interface IBlogPostService
+    {
+        ValueTask<IEnumerable<BlogPost>> GetRecent(int itemCount);
+    }
+}
