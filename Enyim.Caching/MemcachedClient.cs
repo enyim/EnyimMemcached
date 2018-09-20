29fdac53f0d9d8bcdee45b387d28dcd98bb512fc
dudu <dudu@cnblogs.com>
2018-09-20 22:48:57 +0800

    Remove try...catch in GetValueOrCreateAsync

--- Enyim.Caching/MemcachedClient.cs
+++ Enyim.Caching/MemcachedClient.cs
@@ -214,18 +214,11 @@ namespace Enyim.Caching
 
         public async Task<T> GetValueOrCreateAsync<T>(string key, int cacheSeconds, Func<Task<T>> generator)
         {
-            try
+            var result = await GetAsync<T>(key);
+            if (result.Success)
             {
-                var result = await GetAsync<T>(key);
-                if (result.Success)
-                {
-                    _logger.LogDebug($"Cache is hint. Key is '{key}'.");
-                    return result.Value;
-                }
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, $"{nameof(GetAsync)}<{typeof(T)}>(\"{key}\")");
+                _logger.LogDebug($"Cache is hint. Key is '{key}'.");
+                return result.Value;
             }
 
             _logger.LogDebug($"Cache is missed. Key is '{key}'.");
